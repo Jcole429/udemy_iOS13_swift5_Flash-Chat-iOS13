4d9d5643d32616b52c99df8415626fe2ef3ffa41
Jcole429 <Justincole29@hotmail.com>
2020-05-23 17:08:57 -0500

    Lecture 208: Sorting messages by timestamp

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -36,7 +36,9 @@ class ChatViewController: UIViewController {
     }
     
     func loadMessages() {
-        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
             if let e = error {
                 print("Thre was an issue retrieving data from the Firestore. \(e)")
             } else {
@@ -63,7 +65,8 @@ class ChatViewController: UIViewController {
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField: messageSender
-                ,K.FStore.bodyField: messageBody]) { (error) in
+                ,K.FStore.bodyField: messageBody
+                ,K.FStore.dateField: Date().timeIntervalSince1970]) { (error) in
                 if let e = error {
                     print(e)
                 }
